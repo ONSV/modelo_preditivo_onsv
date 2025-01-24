8e972ca983a505eb3a6dd3cbd801b0367edcd83c
Paloma Eleuterio <php.eleuterio@gmail.com>
2025-01-24 09:31:13 -0300

    Update

--- data-raw/datasus.R
+++ data-raw/datasus.R
@@ -5,6 +5,6 @@ obitos_transito <- rtdeaths |>
   count(ano_ocorrencia, name = "mortes") |> 
   rename(ano = ano_ocorrencia) |> 
   drop_na() |> 
-  filter(ano < 2023)
+  filter(ano < 2024)
 
 save(obitos_transito, file = "data/obitos_transito.rda")
